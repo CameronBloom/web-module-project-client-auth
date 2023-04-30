c8e5ddde86dc9ec12f5e23c131e953252f23ecc5
Cameron <cameron.morgan@skiff.com>
2023-04-29 21:08:16 -0500

    Completed the 'FriendsList' component functionality.

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,7 +1,34 @@
 import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios";
 
 export default function FriendsList() {
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios.get("http://localhost:9000/api/friends", {
+      headers: {
+        authorization: token,
+      }
+    })
+      .then(res => setData(res.data))
+      .catch(err => console.error(err))
+  }, []);
+
   return (
-    <div>FriendsList</div>
+    <>
+      <div>FriendsList</div>
+      <ul>
+        {
+          data.map((friend, idx) => {
+            return (
+              <li key={idx}>{friend.name} - {friend.age} - {friend.email}</li>
+            )
+          })
+        }
+      </ul>
+    </>
+
   )
 }
